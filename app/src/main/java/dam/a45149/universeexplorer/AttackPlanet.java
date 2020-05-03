3e035466787db09129a3affd438aba63c6d908fa
yaminyassin <Yamin2008@live.com>
2020-05-03 16:57:51 +0100

    feito o xml do attack activity_attack_planet

---
+++ app/src/main/java/dam/a45149/universeexplorer/AttackPlanet.java
@@ -0,0 +1,14 @@
+package dam.a45149.universeexplorer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AttackPlanet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attack_planet);
+    }
+}
